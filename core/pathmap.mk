ce3c04003fe9027df8b00d93713fd0ae61e3771a
jason <jsnweitzel@gmail.com>
2017-11-04 19:27:53 -0400

    LS rewrites 	modified:   core/Makefile 	modified:   core/config.mk 	modified:   core/dumpvar.mk 	modified:   core/pathmap.mk 	modified:   core/product_config.mk 	modified:   core/soong_config.mk 	modified:   envsetup.sh 	modified:   target/product/core.mk 	modified:   target/product/generic_no_telephony.mk 	modified:   tools/buildinfo.sh
    Change-Id: Ifed3bc43857efd5bfd700faf68bcac2b21a08cce

--- core/pathmap.mk
+++ core/pathmap.mk
@@ -99,6 +99,6 @@ FRAMEWORKS_BASE_SUBDIRS := \
 FRAMEWORKS_BASE_JAVA_SRC_DIRS := \
 	$(addprefix frameworks/base/,$(FRAMEWORKS_BASE_SUBDIRS))
 
-ifneq ($(SLIM_BUILD),)
--include $(TOPDIR)vendor/slim/build/core/pathmap.mk
+ifneq ($(LIQUID_BUILD),)
+-include $(TOPDIR)vendor/liquid/build/core/pathmap.mk
 endif
--- envsetup.sh
+++ envsetup.sh
@@ -23,7 +23,7 @@ Invoke ". build/envsetup.sh" from your shell to add the following functions to y
 
 EOF
 
-    __print_slim_functions_help
+    __print_liquid_functions_help
 
 cat <<EOF
 
@@ -37,7 +37,7 @@ EOF
     local T=$(gettop)
     local A=""
     local i
-    for i in `cat $T/build/envsetup.sh $T/vendor/slim/build/envsetup.sh | sed -n "/^[[:blank:]]*function /s/function \([a-z_]*\).*/\1/p" | sort | uniq`; do
+    for i in `cat $T/build/envsetup.sh $T/vendor/liquid/build/envsetup.sh | sed -n "/^[[:blank:]]*function /s/function \([a-z_]*\).*/\1/p" | sort | uniq`; do
       A="$A $i"
     done
     echo $A
@@ -48,8 +48,8 @@ function build_build_var_cache()
 {
     local T=$(gettop)
     # Grep out the variable names from the script.
-    cached_vars=`cat $T/build/envsetup.sh $T/vendor/slim/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/get_build_var/) print $(i+1)}' | sort -u | tr '\n' ' '`
-    cached_abs_vars=`cat $T/build/envsetup.sh $T/vendor/slim/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/get_abs_build_var/) print $(i+1)}' | sort -u | tr '\n' ' '`
+    cached_vars=`cat $T/build/envsetup.sh $T/vendor/liquid/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/get_build_var/) print $(i+1)}' | sort -u | tr '\n' ' '`
+    cached_abs_vars=`cat $T/build/envsetup.sh $T/vendor/liquid/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/get_abs_build_var/) print $(i+1)}' | sort -u | tr '\n' ' '`
     # Call the build system to dump the "<val>=<value>" pairs as a shell script.
     build_dicts_script=`\cd $T; CALLED_FROM_SETUP=true BUILD_SYSTEM=build/core \
                         command make --no-print-directory -f build/core/config.mk \
@@ -135,13 +135,13 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
-    if (echo -n $1 | grep -q -e "^slim_") ; then
-        SLIM_BUILD=$(echo -n $1 | sed -e 's/^slim_//g')
-        export BUILD_NUMBER=$( (date +%s%N ; echo $SLIM_BUILD; hostname) | openssl sha1 | sed -e 's/.*=//g; s/ //g' | cut -c1-10 )
+    if (echo -n $1 | grep -q -e "^liquid_") ; then
+        LIQUID_BUILD=$(echo -n $1 | sed -e 's/^liquid_//g')
+        export BUILD_NUMBER=$( (date +%s%N ; echo $LIQUID_BUILD; hostname) | openssl sha1 | sed -e 's/.*=//g; s/ //g' | cut -c1-10 )
     else
-        SLIM_BUILD=
+        LIQUID_BUILD=
     fi
-    export SLIM_BUILD
+    export LIQUID_BUILD
 
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
@@ -1702,4 +1702,4 @@ addcompletions
 
 export ANDROID_BUILD_TOP=$(gettop)
 
-. $ANDROID_BUILD_TOP/vendor/slim/build/envsetup.sh
+. $ANDROID_BUILD_TOP/vendor/liquid/build/envsetup.sh
