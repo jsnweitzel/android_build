d900e2ae56a768a207828a816554400346265568
jason <jsnweitzel@gmail.com>
2017-11-03 21:51:19 -0400

    We are LS make flash fun
    Change-Id: Ib9854ebcc1a45ccc4d88bc4a938709bd567f2bdd
    
    	modified:   tools/releasetools/common.py
    	modified:   tools/releasetools/ota_from_target_files.py

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -498,6 +498,19 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   system_progress = 0.75
 
+  script.Print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+  script.Print("     \ )        (          )\ ) )\ )          ")
+  script.Print("     (()/  (   ( )\     (  (()/((()/(         ")
+  script.Print("     (_)) ((_) (_)_)   /((_)(_)) (_))         ")
+  script.Print("     | |   (_) / _ \ (_))( |_ _| |   \        ")
+  script.Print("     | |__ | || (_) || || | | |  | |) |       ")
+  script.Print("     |____||_| \__\_\ \_,_||___| |___/        ")	  	
+  script.Print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+  script.Print("             <%%% Liquid-Oreo %%%>            ")
+  script.Print("           <%%% By LiquidSmooth %%%>          ")
+  script.Print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+  script.Print(" ")
+
   if OPTIONS.wipe_user_data:
     system_progress -= 0.1
   if HasVendorPartition(input_zip):
